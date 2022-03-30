9d6b6c4fa560715aaffc5851c4668bdfee4e6bd1
simra123 <simrafcb@gmail.com>
2022-03-30 05:57:58 +0500

    make it better

--- app.js
+++ app.js
@@ -19,9 +19,6 @@ const io = new Server(http, {
 //allow access
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('the app is up and running')
-})
 
 //port
 const port = process.env.port || 4000
